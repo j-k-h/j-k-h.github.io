15d3e0f7e007b20dfa98432feb86ffb81aa50946
Josh Krauth-Harding <im.joshkh@gmail.com>
2019-04-28 16:19:29 -0400

    Added likemypost

---
+++ likemypost/js/drawEmojis.js
@@ -0,0 +1,43 @@
+let emojis = [];
+let shakeCount = 0;
+
+function initShakeListener() {
+  let shakeEvent = new Shake({
+    threshold: 15
+  });
+  shakeEvent.start();
+
+  window.addEventListener('shake', function() {
+    shake();
+  }, false);
+}
+
+function shake() {
+  emojis = [];
+  for (let i = 0; i < Math.max((50 / (1 + Math.pow((shakeCount + 2) / 5, 5))), 1); i++) {
+    emojis.push(new Emoji(Math.random(), Math.random()));
+  }
+  for (let emoji of emojis) {
+    setTimeout(() => {
+      $("#emoji-container").append(`<div class="emoji ${emoji.sprite}" data-x="${emoji.x}" data-y="${emoji.y}"></div>`);
+      redrawEmojis();
+    }, Math.random() * 1000);
+  }
+  shakeCount++;
+}
+
+function redrawEmojis() {
+  $(".emoji").each((index, emoji) => {
+    $(emoji).css("left", ($(emoji).attr("data-x") * window.innerWidth - 48));
+    $(emoji).css("top", $(emoji).attr("data-y") * window.innerHeight - 48);
+  });
+}
+
+class Emoji {
+  constructor(x, y) {
+    let sprites = ["one", "two", "three", "four"];
+    this.x = x;
+    this.y = y;
+    this.sprite = sprites[Math.floor(Math.random() * sprites.length)];
+  }
+}
--- temp/README.md
+++
@@ -1,4 +0,0 @@
-# Capitalism in Scholarship — Post-Tuition Expenses & Personal Consumerism
-This website explores my personal expenses as a student, designer, and consumer. It also explores the necessity of said expenses — what items did I buy from merely being a consumer and which ones did I need to be successful in art school? This project aims to illuminate the vast amount of consumerism within our society and specifically explores who I am as a customer. Viewers of the website should be able to put themselves in my shoes as a way to address their own consumerism and potentially re-evaluate their spending habits.
-
-The site will organize various products I've bought in a variety of ways such as price, necessity, and time (chronologically).
