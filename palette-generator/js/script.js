1b67b9fabc882bf18d8ff6d2dbf3bdae03dcb70f
Josh Krauth-Harding <hi@jkh.design>
2023-07-11 20:34:38 -0400

    Added save as png functionality

--- palette-generator/js/script.js
+++ palette-generator/js/script.js
@@ -1,6 +1,10 @@
 let i = 1;
 let gradientMode = false;
 
+function invertHex(hex) {
+  return (Number(`0x1${hex}`) ^ 0xFFFFFF).toString(16).substr(1).toUpperCase()
+}
+
 function generateRandomColor() {
   let redValue = Math.floor(Math.random() * (256));
   let redValueHex = redValue.toString(16);
@@ -23,16 +27,18 @@ function generateRandomColor() {
   let colorInRgb = "rgb(" + redValue + ", " + greenValue + ", " + blueValue + ")";
   let colorInHex = "#" + redValueHex + greenValueHex + blueValueHex;
 
-  return [colorInRgb, colorInHex];
+  let oppositeColor = invertHex(redValueHex + greenValueHex + blueValueHex);
+
+  return [colorInRgb, colorInHex, oppositeColor];
 }
 
 function setColor(element) {
   let color = generateRandomColor();
-  element.style.background = "";
-  element.style.backgroundColor = color[0];
-  element.innerHTML = "<span>" + color[1] + "</span>" + "<span class='color-name'>Loading...</span>";
+  let backgroundAndForeground = "background-color: " + color[0] + "; color:#" + color[2];
+  element.setAttribute("style", backgroundAndForeground);
   element.setAttribute("data-hex", color[1]);
-
+  element.innerHTML = "<span>" + color[1] + "</span>" + "<span class='color-name'>Loading...</span>";
+  
   let rawHex = color[1].slice(1);
   fetch("https://api.color.pizza/v1/?values=" + rawHex)
     .then(response => response.json())
@@ -42,8 +48,9 @@ function setColor(element) {
 function setGradient(element) {
   let color1 = generateRandomColor();
   let color2 = generateRandomColor();
+  let colorOpposites = [color1[2], color2[2]];
   element.style.background = "linear-gradient(" + color1[0] + "," + color2[0] + ")";
-  element.innerHTML = "<span class='gradient-colorA'>" + color1[1] + "</span><span class='color-name' id='colorA-name'>Loading...</span><span class='gradient-colorB'>" + color2[1] + "</span><span class='color-name' id='colorB-name'>Loading...</span>";
+  element.innerHTML = "<span class='gradient-colorA' style='color:#" + colorOpposites[0] + "'>" + color1[1] + "</span><span class='color-name' id='colorA-name' style='color:#" + colorOpposites[0] + "'>Loading...</span><span class='gradient-colorB' style='color:#" + colorOpposites[1] + "'>" + color2[1] + "</span><span class='color-name' id='colorB-name' style='color:#" + colorOpposites[1] + "'>Loading...</span>";
   element.setAttribute("data-hex", color1[1] + " → " + color2[1]);
 
   let rawHex1 = color1[1].slice(1);
@@ -62,7 +69,7 @@ function addNewColor() { // ADDS A DIV
   newColor.id = "color" + i;
   newColor.className = "color";
   if (gradientMode == false) {
-    setColor(newColor);;
+    setColor(newColor);
   } else {
     setGradient(newColor);
   }
@@ -81,7 +88,7 @@ function generatePalette() {
 }
 
 window.addEventListener("keydown", event => {
-  if (event.key == "r") {
+  if (event.key == " " || event.key == "r") {
     generatePalette();
   } else if (event.key == "ArrowUp") {
     addNewColor();
@@ -100,6 +107,10 @@ window.addEventListener("keydown", event => {
     generatePalette();
   } else if (event.key == "x") {
     document.getElementById("info").style.display = "none";
+  } else if (event.key == "D" && event.shiftKey) {
+    html2canvas(document.querySelector("#wrapper")).then(canvas => {
+      Canvas2Image.saveAsPNG(canvas);
+  });
   }
 });
 
